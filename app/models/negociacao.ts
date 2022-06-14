9a081ab8368a294fde3947a153d8e06b30d86875
Levy <levyronald3@gmail.com>
2022-06-14 09:10:10 -0300

    feat: CursoTypescript01 Aula03 (completo)

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,26 +1,26 @@
 export class Negociacao{
-    #data;
-    #quantidade;
-    #valor;
+    private _data: Date;
+    private _quantidade: number;
+    private _valor: number;
 
-    constructor(data, quantidade, valor){
-        this.#data = data;
-        this.#quantidade = quantidade;
-        this.#valor = valor;    
+    constructor(data: Date, quantidade: number, valor: number){
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;    
     }
 
     get data(){
-        return this.#data;
+        return this._data;
     }
 
     get quantidade(){
-        return this.#quantidade;
+        return this._quantidade;
     }
 
     get valor(){
-        return this.#valor;
+        return this._valor;
     }
     get volume(){
-        return this.#quantidade * this.#valor;
+        return this._quantidade * this._valor;
     }
 }
